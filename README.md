697fe73785c94177f6b0ebb3a45b7ff1066f8ce0
Khánh Võ An <nhatminhbui2607@gmail.com>
2024-04-02 21:44:32 +0700

    init git

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bùi Minh Nhật - 2011060734
+<h1>Dop dop yes</h1>
---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: 'TUNG123',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 25,
+    Username: "d73aa3ea5a7b77",
+    Password: "2b968a4158d8e2"
+}
